dda03f880ac03efb17a6b4e0c0bcaf99d939f956
zbx <sjtuzhangbenxuan@163.com>
2018-12-21 15:33:24 +0800

    add kalman filter

---
+++ MOSSE/mosse.py
@@ -0,0 +1,100 @@
+import numpy as np
+import cv2
+import os
+import argparse
+
+def linear_mapping(images):
+    max_value = images.max()
+    min_value = images.min()
+
+    parameter_a = 1 / (max_value - min_value)
+    parameter_b = 1 - max_value * parameter_a
+
+    image_after_mapping = parameter_a * images + parameter_b
+
+    return image_after_mapping
+
+# pre-processing the image...
+def pre_process(img):
+    # get the size of the img...
+    height, width = img.shape
+    img = np.log(img + 1)
+    img = (img - np.mean(img)) / (np.std(img) + 1e-5)
+    # use the hanning window...
+    window = window_func_2d(height, width)
+    img = img * window
+
+    return img
+
+def window_func_2d(height, width):
+    win_col = np.hanning(width)
+    win_row = np.hanning(height)
+    mask_col, mask_row = np.meshgrid(win_col, win_row)
+
+    win = mask_col * mask_row
+
+    return win
+
+def random_warp(img):
+    a = -180 / 16
+    b = 180 / 16
+    r = a + (b - a) * np.random.uniform()
+    # rotate the image...
+    matrix_rot = cv2.getRotationMatrix2D((img.shape[1]/2, img.shape[0]/2), r, 1)
+    img_rot = cv2.warpAffine(np.uint8(img * 255), matrix_rot, (img.shape[1], img.shape[0]))
+    img_rot = img_rot.astype(np.float32) / 255
+    return img_rot
+
+def get_img_lists(img_path):
+    frame_list = []
+    for frame in os.listdir(img_path):
+        if os.path.splitext(frame)[1] == '.jpg':
+            frame_list.append(os.path.join(img_path, frame))
+    return frame_list
+
+class mosse:
+    def __init__(self, args, img_path):
+        self.args     = args
+        self.img_path_ = img_path
+        self.frame_lists = get_img_lists(img_path)
+        self.frame_lists.sort()
+
+    def start_tracking(self):
+        init_img = cv2.imread(self.frame_lists[0])
+        init_frame = cv2.cvtColor(init_img, cv2.COLOR_BGR2GRAY)
+        init_frame = init_frame.astype(np.float32)
+        init_gt = cv2.selectROI('demo', init_img, False, False)
+        init_gt = np.array(init_gt).astype(np.int64)
+        response_map = self._get_gauss_response(init_frame, init_gt)
+        cv2.waitKey(100)
+        return response_map
+    
+    def _get_gauss_response(self, img, gt):
+        # get the shape of the image..
+        height, width = img.shape
+        # get the mesh grid...
+        xx, yy = np.meshgrid(np.arange(width), np.arange(height))
+        # get the center of the object...
+        center_x = gt[0] + 0.5 * gt[2]
+        center_y = gt[1] + 0.5 * gt[3]
+        # cal the distance...
+        dist = (np.square(xx - center_x) + np.square(yy - center_y)) / (2 * self.args.sigma)
+        # get the response map...
+        response = np.exp(-dist)
+        # normalize...
+        response =  (response)
+        return response
+
+
+parse = argparse.ArgumentParser()
+parse.add_argument('--lr', type=float, default=0.125, help='the learning rate')
+parse.add_argument('--sigma', type=float, default=100, help='the sigma')
+parse.add_argument('--num_pretrain', type=int, default=128, help='the number of pretrain')
+parse.add_argument('--rotate', action='store_true', help='if rotate image during pre-training.')
+parse.add_argument('--record', action='store_true', help='record the frames')
+
+if __name__ == '__main__':
+    img_path = "../../data/pingpong"
+    args = parse.parse_args()
+    tracker = mosse(args, img_path)
+    response_map = tracker.start_tracking()
